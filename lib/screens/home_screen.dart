2905839f7491012e9c878b88f11b24c7f22b04bd
SoheilKhorram <soheil13261428@gmail.com>
2023-05-23 01:22:28 +0430

    basic styling of the buttons and their box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,11 +15,11 @@ class HomeScreen extends StatelessWidget {
               child: Transform.translate(
                 offset: const Offset(0, 52),
                 child: Container(
-                  height: 150,
+                  height: 130,
                   color: const Color(0xFFFDB713),
                   child: Center(
                     child: Transform.translate(
-                      offset: const Offset(0, -20),
+                      offset: const Offset(0, -10),
                       child: const Text(
                         'پرواز',
                         style: TextStyle(
@@ -34,14 +34,60 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: 165,
+              top: 155,
               left: 40,
               right: 40,
-              height: 75,
+              height: 50,
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
+                borderRadius: BorderRadius.circular(10),
                 child: Container(
-                  color: Colors.grey,
+                  color: const Color(0xFFFDB713),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'پرواز خارجی',
+                            style: TextStyle(
+                              fontFamily: 'Vazir',
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'پرواز داخلی',
+                            style: TextStyle(
+                              fontFamily: 'Vazir',
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
