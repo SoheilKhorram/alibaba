1ba298b60a5762ba243bc067c0de0c85284ddbd1
SoheilKhorram <soheil13261428@gmail.com>
2023-05-22 09:11:18 +0430

    adding visibility icon to the password input

--- lib/widgets/password_input.dart
+++ lib/widgets/password_input.dart
@@ -12,13 +12,13 @@ class PasswordInput extends StatefulWidget {
 class _PasswordInputState extends State<PasswordInput> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         child: Directionality(
           textDirection: TextDirection.rtl,
           child: TextField(
-            style: TextStyle(
+            style: const TextStyle(
               fontFamily: 'Vazir',
               fontSize: 16,
             ),
@@ -26,21 +26,26 @@ class _PasswordInputState extends State<PasswordInput> {
             cursorHeight: 24,
             cursorWidth: 1,
             decoration: InputDecoration(
-              prefixIcon: Icon(
+              prefixIcon: const Icon(
                 Icons.lock_outline,
                 color: Colors.grey,
               ),
-              border: OutlineInputBorder(
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.visibility),
+              ),
+              border: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(8)),
               ),
-              focusedBorder: OutlineInputBorder(
+              focusedBorder: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(8)),
                 borderSide: BorderSide(
                   color: Color(0xFFFDB713),
                   width: 2.5,
                 ),
               ),
-              labelStyle: TextStyle(fontFamily: 'Vazir', color: Colors.grey),
+              labelStyle:
+                  const TextStyle(fontFamily: 'Vazir', color: Colors.grey),
               labelText: 'کلمه عبور',
             ),
           ),
