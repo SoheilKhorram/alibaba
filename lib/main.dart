c6c49cb00b480dfcc35386ba303549887c3f6e3d
SoheilKhorram <soheil13261428@gmail.com>
2023-05-22 23:27:36 +0430

    creating login screen by changing the signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:alibaba/screens/signup_screen.dart';
+import 'package:alibaba/screens/login_screen.dart';
 
 void main() {
   runApp(
-    const SignupScreen(),
+    const LoginScreen(),
   );
 }
