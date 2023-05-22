631fdf606adeba43b320bde61554809ecccea006
SoheilKhorram <soheil13261428@gmail.com>
2023-05-23 00:28:16 +0430

    making the internal and international flies style

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,17 +8,40 @@ class HomeScreen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: const [
-            SizedBox(height: 52),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-              child: Text(
-                'پرواز',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Vazir',
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Transform.translate(
+                offset: const Offset(0, 52),
+                child: Container(
+                  height: 150,
+                  color: const Color(0xFFFDB713),
+                  child: Center(
+                    child: Transform.translate(
+                      offset: const Offset(0, -20),
+                      child: const Text(
+                        'پرواز',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Vazir',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 165,
+              left: 40,
+              right: 40,
+              height: 75,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  color: Colors.grey,
                 ),
               ),
             ),
