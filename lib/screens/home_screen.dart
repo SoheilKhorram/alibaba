96e2a278dd18f8678b108f6a5f8cac7842c7280c
SoheilKhorram <soheil13261428@gmail.com>
2023-05-23 17:33:48 +0430

    creating the drop down menus

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'package:alibaba/widgets/fly_button.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
+    ColorLabel? selectedColor;
+    final List<DropdownMenuEntry<ColorLabel>> colorEntries =
+        <DropdownMenuEntry<ColorLabel>>[];
+    for (final ColorLabel color in ColorLabel.values) {
+      colorEntries.add(
+        DropdownMenuEntry<ColorLabel>(value: color, label: color.label),
+      );
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -54,9 +70,58 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 220,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: DropdownMenu(
+                      width: 165,
+                      label: const Text('Color'),
+                      dropdownMenuEntries: colorEntries,
+                      onSelected: (ColorLabel? color) {
+                        setState(() {
+                          selectedColor = color;
+                        });
+                      },
+                    ),
+                  ),
+                  Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: DropdownMenu(
+                      width: 165,
+                      initialSelection: ColorLabel.green,
+                      label: const Text('Color'),
+                      dropdownMenuEntries: colorEntries,
+                      onSelected: (ColorLabel? color) {
+                        setState(() {
+                          selectedColor = color;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+enum ColorLabel {
+  blue('Blue', Colors.blue),
+  pink('Pink', Colors.pink),
+  green('Green', Colors.green),
+  yellow('Yellow', Colors.yellow),
+  grey('Grey', Colors.grey);
+
+  const ColorLabel(this.label, this.color);
+  final String label;
+  final Color color;
 }
