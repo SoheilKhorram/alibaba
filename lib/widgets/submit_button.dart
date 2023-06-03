6dd84bcbef77d0278c890f489b6d6076c1052dd0
SoheilKhorram <soheil13261428@gmail.com>
2023-06-03 09:34:40 +0430

    removing the padding from the button itself

--- lib/widgets/submit_button.dart
+++ lib/widgets/submit_button.dart
@@ -8,27 +8,22 @@ class SubmitButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFFFDB713),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 16),
-            ),
-            onPressed: () {},
-            child: Text(
-              text,
-              style: const TextStyle(
-                color: Colors.black,
-                fontFamily: 'Vazir',
-                fontSize: 16,
-              ),
-            ),
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFFFDB713),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+        ),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black,
+            fontFamily: 'Vazir',
+            fontSize: 18,
           ),
         ),
       ),
