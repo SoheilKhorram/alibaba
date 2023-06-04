55b501071deb8d3b014a05f699a9e368efa71691
SoheilKhorram <soheil13261428@gmail.com>
2023-06-04 19:57:14 +0430

    trying to make a horizontal list

--- lib/screens/search_result_screen.dart
+++ lib/screens/search_result_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:alibaba/widgets/ticket_app_bar.dart';
+//import 'package:alibaba/widgets/ticket_app_bar.dart';
 
 class SearchResultScreen extends StatefulWidget {
   const SearchResultScreen({super.key});
@@ -14,8 +14,24 @@ class SearchResultScreen extends StatefulWidget {
 class _SearchResultScreenState extends State<SearchResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TicketAppBar(),
+    return Scaffold(
+      //appBar: const TicketAppBar(),
+      body: Column(
+        children: [
+          ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                  leading: const Icon(Icons.list),
+                  trailing: const Text(
+                    "GFG",
+                    style: TextStyle(color: Colors.green, fontSize: 15),
+                  ),
+                  title: Text("List item $index"));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
