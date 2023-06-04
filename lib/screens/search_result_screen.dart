b03a617e5cc284b6c1b1991921baf40fd4a0c4af
SoheilKhorram <soheil13261428@gmail.com>
2023-06-04 12:27:22 +0430

    creating the app bar widget

--- lib/screens/search_result_screen.dart
+++ lib/screens/search_result_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:alibaba/widgets/travel_info_text.dart';
-import 'package:alibaba/widgets/travel_info_date.dart';
+import 'package:alibaba/widgets/ticket_app_bar.dart';
 
 class SearchResultScreen extends StatefulWidget {
   const SearchResultScreen({super.key});
@@ -15,40 +14,8 @@ class SearchResultScreen extends StatefulWidget {
 class _SearchResultScreenState extends State<SearchResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 75,
-        backgroundColor: Colors.white,
-        actions: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TravelInfoText(
-                transportationVehicle: "هواپیما",
-                originCity: "تهران",
-                destinationCity: "مشهد",
-              ),
-              TravelInfoDate(date: "چهارشنبه ۲۵ اسفند")
-            ],
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.arrow_forward, color: Colors.black54),
-          ),
-        ],
-        leading: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: CircleAvatar(
-            backgroundColor: const Color(0xFFFDB713),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search, color: Colors.black),
-            ),
-          ),
-        ),
-        leadingWidth: 75,
-      ),
+    return const Scaffold(
+      appBar: TicketAppBar(),
     );
   }
 }
---
+++ lib/widgets/ticket_app_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:alibaba/widgets/travel_info_text.dart';
+import 'package:alibaba/widgets/travel_info_date.dart';
+
+class TicketAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TicketAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      toolbarHeight: 150,
+      backgroundColor: Colors.white,
+      actions: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TravelInfoText(
+              transportationVehicle: "هواپیما",
+              originCity: "تهران",
+              destinationCity: "مشهد",
+            ),
+            TravelInfoDate(date: "چهارشنبه ۲۵ اسفند")
+          ],
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_forward, color: Colors.black54),
+        ),
+      ],
+      leading: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: CircleAvatar(
+          backgroundColor: const Color(0xFFFDB713),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, color: Colors.black),
+          ),
+        ),
+      ),
+      leadingWidth: 75,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(75);
+}
