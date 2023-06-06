7830af5a1cadde6282c55fa577b64824bae03f87
Mani <maniam82p@gmail.com>
2023-06-06 18:58:58 +0330

    control the validation of email input

--- lib/widgets/email_input.dart
+++ lib/widgets/email_input.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 
-class EmailInput extends StatelessWidget {
-  const EmailInput({super.key});
+class EmailInput extends StatefulWidget {
+  const EmailInput({Key? key}) : super(key: key);
+
+  @override
+  _EmailInputState createState() => _EmailInputState();
+}
+
+class _EmailInputState extends State<EmailInput> {
+  final TextEditingController _emailController = TextEditingController();
+  bool _showError = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  bool _validateEmail(String value) {
+    final emailRegex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$';
+    return RegExp(emailRegex).hasMatch(value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         child: Directionality(
           textDirection: TextDirection.rtl,
           child: TextField(
-            style: TextStyle(
+            controller: _emailController,
+            onChanged: (value) {
+              setState(() {
+                _showError = !_validateEmail(value);
+              });
+            },
+            onEditingComplete: () {
+              setState(() {
+                _showError = !_validateEmail(_emailController.text);
+              });
+            },
+            style: const TextStyle(
               fontFamily: 'Vazir',
               fontSize: 16,
             ),
@@ -19,22 +49,28 @@ class EmailInput extends StatelessWidget {
             cursorHeight: 24,
             cursorWidth: 1,
             decoration: InputDecoration(
-              prefixIcon: Icon(
+              prefixIcon: const Icon(
                 Icons.mail_outline,
                 color: Colors.grey,
               ),
-              border: OutlineInputBorder(
+              border: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(8)),
               ),
-              focusedBorder: OutlineInputBorder(
+              focusedBorder: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(8)),
                 borderSide: BorderSide(
                   color: Color(0xFFFDB713),
                   width: 2.5,
                 ),
               ),
-              labelStyle: TextStyle(fontFamily: 'Vazir', color: Colors.grey),
+              labelStyle: const TextStyle(fontFamily: 'Vazir', color: Colors.grey),
               labelText: 'ایمیل',
+              errorText: _showError ? 'ایمیل نامعتبر است' : null,
+              errorStyle: const TextStyle(
+                fontFamily: 'Vazir',
+                fontSize: 12,
+                color: Colors.red,
+              ),
             ),
           ),
         ),
