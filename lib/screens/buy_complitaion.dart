f88bf06f6c4d4e33fcc73250eaa37b0fc5375abc
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-06-07 19:34:15 +0330

    create page complition

---
+++ lib/screens/buy_complitaion.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BuyComplition extends StatefulWidget {
+  const BuyComplition({super.key});
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
