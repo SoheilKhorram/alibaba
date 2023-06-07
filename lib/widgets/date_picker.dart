754ccda5fecf3e9faee52bdec5f4065f33ed062a
Mani <maniam82p@gmail.com>
2023-06-07 16:07:07 +0330

    doing some change to add date picker (not primary)

--- lib/widgets/date_picker.dart
+++ lib/widgets/date_picker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
 
 import 'package:alibaba/widgets/fly_button.dart';
 import 'package:alibaba/widgets/submit_button.dart';
@@ -6,8 +7,70 @@ import 'package:alibaba/widgets/travel_city.dart';
 import 'package:alibaba/widgets/travel_date.dart';
 import 'package:alibaba/widgets/traveler_input.dart';
 
-import '../widgets/date_picker.dart';
+class DatePickerButton extends StatefulWidget {
+  final void Function(DateTime selectedDate)? onDateSelected;
 
+  const DatePickerButton({Key? key, this.onDateSelected}) : super(key: key);
+
+  String get text => "تاریخ برگشت";
+
+
+  @override
+  _DatePickerButtonState createState() => _DatePickerButtonState();
+}
+
+class _DatePickerButtonState extends State<DatePickerButton> {
+  DateTime? selectedDate;
+
+  void _showDatePicker() async {
+    var picked = await showPersianDatePicker(
+      context: context,
+      initialDate: Jalali.now(),
+      firstDate: Jalali(1385, 8),
+      lastDate: Jalali(1450, 9),
+    );
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked.toDateTime();
+        if (widget.onDateSelected != null) {
+          widget.onDateSelected!(selectedDate!);
+        }
+      });
+    }
+  }
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _showDatePicker,
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: OutlinedButton.icon(
+          icon: const Icon(
+            Icons.calendar_month_outlined,
+            color: Colors.grey,
+            size: 20,
+          ),
+          label: Text(
+            widget.text,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontFamily: 'Vazir',
+              fontSize: 15,
+            ),
+          ),
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            backgroundColor: const Color(0x05000000),
+            side: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key});
@@ -75,13 +138,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 borderRadius: BorderRadius.circular(10),
                 child: Container(
                   color: Colors.white,
-                  child: Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        FlyButton('پرواز خارجی'),
-                      ],
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      FlyButton('پرواز خارجی'),
+                    ],
                   ),
                 ),
               ),
