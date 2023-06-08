5941d1c79fc806d500cd8bbfd7bd3cd6561b7aff
SoheilKhorram <soheil13261428@gmail.com>
2023-06-08 19:41:05 +0430

    removing the Container widget

--- lib/widgets/ticket_avalibility.dart
+++ lib/widgets/ticket_avalibility.dart
@@ -19,47 +19,45 @@ class TicketAvalibility extends StatelessWidget {
     if (isAvailable) {
       return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Container(
-          child: Row(
-            children: [
-              const Text(
-                'تومان',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Color(0xff959ea6),
-                  fontFamily: 'Vazir',
-                ),
+        child: Row(
+          children: [
+            const Text(
+              'تومان',
+              style: TextStyle(
+                fontSize: 12,
+                color: Color(0xff959ea6),
+                fontFamily: 'Vazir',
               ),
-              const SizedBox(width: 4),
-              Text(
-                price!.toPersianDigit().seRagham(),
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Vazir',
-                  fontWeight: FontWeight.w900,
-                  color: Color(0xff0077db),
-                ),
+            ),
+            const SizedBox(width: 4),
+            Text(
+              price!.toPersianDigit().seRagham(),
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'Vazir',
+                fontWeight: FontWeight.w900,
+                color: Color(0xff0077db),
               ),
-              const Spacer(),
-              const Text(
-                'صندلی باقی مانده',
-                style: TextStyle(
-                  fontSize: 10,
-                  fontFamily: 'Vazir',
-                  color: Color(0xffdc3545),
-                ),
+            ),
+            const Spacer(),
+            const Text(
+              'صندلی باقی مانده',
+              style: TextStyle(
+                fontSize: 10,
+                fontFamily: 'Vazir',
+                color: Color(0xffdc3545),
               ),
-              const SizedBox(width: 4),
-              Text(
-                numberOfRemainingSeats!.toPersianDigit(),
-                style: const TextStyle(
-                  fontSize: 10,
-                  fontFamily: 'Vazir',
-                  color: Color(0xffdc3545),
-                ),
+            ),
+            const SizedBox(width: 4),
+            Text(
+              numberOfRemainingSeats!.toPersianDigit(),
+              style: const TextStyle(
+                fontSize: 10,
+                fontFamily: 'Vazir',
+                color: Color(0xffdc3545),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
     } else {
