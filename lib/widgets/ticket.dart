5a6b03e6c1ff3ba03565d6173f8d8655fad8681f
SoheilKhorram <soheil13261428@gmail.com>
2023-06-08 12:18:06 +0430

    done working on the prices

--- lib/widgets/ticket.dart
+++ lib/widgets/ticket.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:alibaba/widgets/tag.dart';
+import 'package:alibaba/widgets/ticket_tag.dart';
 import 'package:alibaba/widgets/ticket_line.dart';
 import 'package:alibaba/widgets/airplane_logo.dart';
 import 'package:alibaba/widgets/ticket_time.dart';
+import 'package:alibaba/widgets/ticket_avalibility.dart';
 
 class Ticket extends StatelessWidget {
   const Ticket({super.key});
@@ -14,36 +15,50 @@ class Ticket extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         border: Border.all(
-          color: Colors.grey,
+          color: const Color(0x25000000),
           width: 1,
         ),
         borderRadius: const BorderRadius.all(
           Radius.circular(10),
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      child: Column(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [Tag('سیستمی'), Tag('اکونومی'), Tag('M37')],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: const [
-                  TicketTime(time: '22:30'),
-                  Line(),
-                  TicketTime(time: '22:30'),
-                ],
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [Tag('سیستمی'), Tag('اکونومی'), Tag('M37')],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: const [
+                        TicketTime(time: '22:30'),
+                        Line(),
+                        TicketTime(time: '22:30'),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 16),
+                const AirplaneLogo()
+              ],
+            ),
           ),
-          const SizedBox(width: 16),
-          const AirplaneLogo()
+          const SizedBox(
+              height: 1,
+              width: double.infinity,
+              child: DecoratedBox(
+                decoration: BoxDecoration(color: Color(0x25000000)),
+              )),
+          TicketAvalibility(price: '1200000', numberOfRemainingSeats: '7'),
         ],
       ),
     );
---
+++ lib/widgets/ticket_avalibility.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:persian_number_utility/persian_number_utility.dart';
+
+class TicketAvalibility extends StatelessWidget {
+  TicketAvalibility({
+    super.key,
+    this.price,
+    this.numberOfRemainingSeats,
+    this.isAvailable,
+  });
+
+  String? price;
+  String? numberOfRemainingSeats;
+  bool? isAvailable = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        children: [
+          const Text(
+            'تومان',
+            style: TextStyle(
+              fontSize: 12,
+              color: Color(0xff959ea6),
+              fontFamily: 'Vazir',
+            ),
+          ),
+          const SizedBox(width: 4),
+          Text(
+            price!.toPersianDigit().seRagham(),
+            style: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'Vazir',
+              fontWeight: FontWeight.w900,
+              color: Color(0xff0077db),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
