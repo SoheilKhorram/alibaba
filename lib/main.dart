bba01a0a3e1a51eb3db8a5d3fa5ebb1e4f8baf6a
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-06-09 18:12:40 +0330

    add page in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:alibaba/screens/signup_screen.dart';
 import 'package:alibaba/screens/login_screen.dart';
 import 'package:alibaba/screens/home_screen.dart';
+import 'package:alibaba/screens/buy_complitaion.dart';
+import 'package:alibaba/screens/my_steper_page.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    ),
+    MySteperPage(),
   );
 }
