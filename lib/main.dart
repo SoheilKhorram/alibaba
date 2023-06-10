9937093c35c0c9f439cffca2cc98a66d7351bd35
SoheilKhorram <soheil13261428@gmail.com>
2023-06-10 14:43:17 +0430

    Revert "Merge branch 'main' into home_screen"
    This reverts commit c312e2974aac15253850a5e14a39f0e28b5a5ac4.

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:alibaba/screens/signup_screen.dart';
 import 'package:alibaba/screens/login_screen.dart';
 import 'package:alibaba/screens/home_screen.dart';
-import 'package:alibaba/screens/search_result_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SearchResultScreen(),
+      home: HomeScreen(),
     ),
   );
 }
