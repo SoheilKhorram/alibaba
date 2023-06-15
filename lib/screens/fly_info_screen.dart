8005cc4bc0c7bb3fb52c6418912ef2f021327311
SoheilKhorram <soheil13261428@gmail.com>
2023-06-15 20:13:55 +0430

    adding the flight date and time to the screen

--- lib/screens/fly_info_screen.dart
+++ lib/screens/fly_info_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:alibaba/widgets/fly_info_app_bar.dart';
 import 'package:alibaba/widgets/airplane_logo.dart';
 import 'package:alibaba/widgets/fly_info_line.dart';
+import 'package:persian_number_utility/persian_number_utility.dart';
 
 class FlyInfoScreen extends StatelessWidget {
   const FlyInfoScreen({super.key});
@@ -18,9 +19,66 @@ class FlyInfoScreen extends StatelessWidget {
             Row(
               textDirection: TextDirection.rtl,
               mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                AirplaneLogo(),
-                Line(),
+              children: [
+                const AirplaneLogo(),
+                const Line(),
+                Column(
+                  children: [
+                    Text(
+                      '19:55'.toPersianDigit(),
+                      style: const TextStyle(
+                        fontFamily: 'Vazir',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 55),
+                    Text(
+                      '19:55'.toPersianDigit(),
+                      style: const TextStyle(
+                        fontFamily: 'Vazir',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'شیراز',
+                      style: TextStyle(
+                        fontFamily: 'Vazir',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'پنجشنبه, 15 تیر'.toPersianDigit(),
+                      style: const TextStyle(
+                        fontFamily: 'Vazir',
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      'تهران',
+                      style: TextStyle(
+                        fontFamily: 'Vazir',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'پنجشنبه, 15 تیر'.toPersianDigit(),
+                      style: const TextStyle(
+                        fontFamily: 'Vazir',
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ],
