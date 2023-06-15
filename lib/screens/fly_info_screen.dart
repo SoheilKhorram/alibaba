43a36d840433dd25e87942e9cb2bc56330c16aaf
SoheilKhorram <soheil13261428@gmail.com>
2023-06-15 19:06:01 +0430

    making the line

--- lib/screens/fly_info_screen.dart
+++ lib/screens/fly_info_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 import 'package:alibaba/widgets/fly_info_app_bar.dart';
+import 'package:alibaba/widgets/airplane_logo.dart';
+import 'package:alibaba/widgets/fly_info_line.dart';
 
 class FlyInfoScreen extends StatelessWidget {
   const FlyInfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: FlyInfoAppBar(),
+    return Scaffold(
+      appBar: const FlyInfoAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        child: Column(
+          children: [
+            Row(
+              textDirection: TextDirection.rtl,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                AirplaneLogo(),
+                Line(),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
