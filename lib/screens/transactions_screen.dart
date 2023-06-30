13fa540686d6f70954ca33382702fd83e263e664
SoheilKhorram <soheil13261428@gmail.com>
2023-06-30 11:46:57 +0430

    working on the card of the screen

--- lib/screens/transactions_screen.dart
+++ lib/screens/transactions_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:alibaba/widgets/transactions_screen_app_bar.dart';
+import 'package:alibaba/widgets/transactions_card.dart';
 
 class TransactionsScreen extends StatelessWidget {
   const TransactionsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TransactionsScreenAppBar(),
-      body: Center(
-        child: Text("Transactions Screen"),
+    return Scaffold(
+      appBar: const TransactionsScreenAppBar(),
+      body: Column(
+        children: const [
+          TransactionsCard(),
+        ],
       ),
     );
   }
---
+++ lib/widgets/transactions_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TransactionsCard extends StatelessWidget {
+  const TransactionsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.red,
+    );
+  }
+}
