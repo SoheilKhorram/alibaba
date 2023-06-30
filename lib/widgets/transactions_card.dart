79a5e434a954ae9c87ed99178f1ec13506c7f122
SoheilKhorram <soheil13261428@gmail.com>
2023-06-30 11:49:21 +0430

    some style for the card

--- lib/widgets/transactions_card.dart
+++ lib/widgets/transactions_card.dart
@@ -6,9 +6,56 @@ class TransactionsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: 100,
-      color: Colors.red,
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.only(bottom: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      alignment: Alignment.centerRight,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            '$date    :تاریخ',
+            style: const TextStyle(
+              fontSize: 16,
+              fontFamily: 'Vazir',
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            '$orderNumber    :شماره سفارش',
+            style: const TextStyle(
+              fontSize: 16,
+              fontFamily: 'Vazir',
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'نوع سفارش:    $orderType',
+            style: const TextStyle(
+              fontSize: 16,
+              fontFamily: 'Vazir',
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            '$price    :قیمت',
+            style: const TextStyle(
+              fontSize: 16,
+              fontFamily: 'Vazir',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
