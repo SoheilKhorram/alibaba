8836ed7d9cb1f2d73e081a5993677d439c6144d5
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-07-01 14:36:55 +0330

    add onpreessed

--- lib/widgets/submit_button.dart
+++ lib/widgets/submit_button.dart
@@ -1,33 +1,33 @@
-import 'package:flutter/material.dart';
-
-class SubmitButton extends StatelessWidget {
-  const SubmitButton(this.text, {super.key, this.backgroundColor});
-
-  final String text;
-  final Color? backgroundColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor ?? const Color(0xFFFDB713),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-          ),
-          padding: const EdgeInsets.symmetric(vertical: 16),
-        ),
-        onPressed: () {},
-        child: Text(
-          text,
-          style: TextStyle(
-            color: backgroundColor != null ? Colors.white : Colors.black,
-            fontFamily: 'Vazir',
-            fontSize: 18,
-          ),
-        ),
-      ),
-    );
-  }
+import 'package:flutter/material.dart'; 
+ 
+class SubmitButton extends StatelessWidget { 
+  const SubmitButton(this.text, {super.key, this.onPressed}); 
+ 
+  final String text; 
+  final VoidCallback? onPressed; 
+ 
+  @override 
+  Widget build(BuildContext context) { 
+    return SizedBox( 
+      width: double.infinity, 
+      child: ElevatedButton( 
+        style: ElevatedButton.styleFrom( 
+          backgroundColor: const Color(0xFFFDB713), 
+          shape: const RoundedRectangleBorder( 
+            borderRadius: BorderRadius.all(Radius.circular(8)), 
+          ), 
+          padding: const EdgeInsets.symmetric(vertical: 16), 
+        ), 
+        onPressed: onPressed, 
+        child: Text( 
+          text, 
+          style: const TextStyle( 
+            color: Colors.black, 
+            fontFamily: 'Vazir', 
+            fontSize: 18, 
+          ), 
+        ), 
+      ), 
+    ); 
+  } 
 }
