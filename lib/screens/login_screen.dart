c90cdd25ab8f376d8f6a6410d9cb452ab7774933
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-07-02 04:05:59 +0330

    respince back for login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,12 +1,44 @@
+import 'dart:io';
+import 'package:alibaba/screens/home_screen.dart';
+import 'package:alibaba/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
-
 import 'package:alibaba/widgets/name_input.dart';
 import 'package:alibaba/widgets/password_input.dart';
-import 'package:alibaba/widgets/email_input.dart';
 import 'package:alibaba/widgets/submit_button.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+String response = '', message = '';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<String> LoginScreen(String userName, String password) async {
+    await Socket.connect("10.0.2.2", 4321).then((serverSocket) {
+      serverSocket.write('signin/$userName/$password\u0000');
+      serverSocket.flush();
+      serverSocket.listen((socket) {
+        setState(() {
+          response = String.fromCharCodes(socket);
+        });
+      });
+    });
+    return response;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +48,9 @@ class LoginScreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(15),
           child: Column(
-            children: const [
-              SizedBox(height: 52),
-              Text(
+            children: [
+              const SizedBox(height: 52),
+              const Text(
                 'ورود با کلمه عبور',
                 style: TextStyle(
                   fontSize: 22,
@@ -26,14 +58,63 @@ class LoginScreen extends StatelessWidget {
                   fontFamily: 'Vazir',
                 ),
               ),
-              SizedBox(height: 15),
-              NameInput(),
-              SizedBox(height: 15),
-              PasswordInput(),
-              SizedBox(height: 15),
-              EmailInput(),
-              Spacer(),
-              SubmitButton('ورود به علی‌بابا'),
+              const SizedBox(height: 15),
+              NameInput(controller: _nameController),
+              const SizedBox(height: 15),
+              PasswordInput(controller: _passwordController),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  const SizedBox(width: 25),
+                  const Text("اکانت ندارید :"),
+                  TextButton(
+                    child: const Text(
+                      'ثبت نام کنید',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SignupScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  Text(message),
+                ],
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SubmitButton(
+                    'ورود در علی‌بابا',
+                    onPressed: () async {
+                      String name = _nameController.text;
+                      String password = _passwordController.text;
+                      result = await LoginScreen(
+                        name,
+                        password,
+                      );
+                      if (result == '1') {
+                        message = 'وارد شدید';
+                        // User.name = name;
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomeScreen(),
+                          ),
+                        );
+                      } else {
+                        // ToDo : show error message that user name is duplicate
+                        message = 'اطلاعات وارد شده اشتباه است';
+                      }
+                    },
+                  ),
+                ),
+              ),
+              // Spacer(),
+              // SubmitButton('ورود به علی‌بابا'),
             ],
           ),
         ),
