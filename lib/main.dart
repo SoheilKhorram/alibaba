f19a16be22b25eaa8e6bf481802ee44f43c2e22e
SoheilKhorram <soheil13261428@gmail.com>
2023-07-02 12:38:48 +0430

    some minor change to the main file

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,21 @@ import 'package:alibaba/screens/search_result_screen.dart';
 import 'package:alibaba/screens/flight_info_screen.dart';
 import 'package:alibaba/screens/info_confirmation_screen.dart';
 import 'package:alibaba/screens/account_info_screen.dart';
+<<<<<<< HEAD
 import 'package:alibaba/screens/user_vacations_screen.dart';
+=======
 import 'package:alibaba/screens/transactions_screen.dart';
+>>>>>>> transactions_screen
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
+<<<<<<< HEAD
       home: UserVacationsScreen(),
+=======
       home: TransactionsScreen(),
+>>>>>>> transactions_screen
     ),
   );
 }
